805ef37303b24fea33cc59c0cbdee0f94c6bdf76
ituitis-beydemir18 <56716335+ituitis-beydemir18@users.noreply.github.com>
2021-04-07 11:50:11 +0300

    Add files via upload

--- instagram.py
+++ instagram.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import time as tm
 from selenium.webdriver.common.keys import Keys
+#import emoji
 driver = webdriver.Chrome(r"C:\Users\User\OneDrive\Masaüstü\chromedriver.exe")
 
 driver.get("https://www.instagram.com")
@@ -15,32 +16,71 @@ sifre1.send_keys("3610619Baris")
 driver.find_element_by_xpath("//*[@id='loginForm']/div/div[3]/button/div").click()
 tm.sleep(5)
 driver.find_element_by_xpath("//*[@id='react-root']/section/nav/div[2]/div/div/div[2]/input").send_keys("askidanevar")
-tm.sleep(2)
+tm.sleep(3)
 driver.find_element_by_xpath("//*[@id='react-root']/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/a/div").click()
-tm.sleep(2)
+tm.sleep(3)
 
-tm.sleep(4)
-driver.execute_script("window.scrollTo(0, 2000)")                        #bu kısım yüklenmemiş resme basılamayacağı için onun yüklenmesini sağlıyordu
-tm.sleep(4)
-driver.execute_script("window.scrollTo(0, 2000)")
-driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/div[3]/article/div[1]/div/div[4]/div[2]/a/div").click()
+"""tm.sleep(5)
+driver.execute_script("window.scrollTo(0, 2000)")                                                                                                     #bu kısım yüklenmemiş resme basılamayacağı için onun yüklenmesini sağlıyordu
+tm.sleep(5)
+driver.execute_script("window.scrollTo(0, 2000)")"""
+driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/div[3]/article/div[1]/div/div[2]/div[2]/a/div").click()
 
 
 tm.sleep(5)
-driver.find_element_by_xpath("/html/body/div[5]/div[2]/div/article/div[3]/div[1]/ul/li/div/button/span").click()
+driver.find_element_by_xpath("/html/body/div[5]/div[2]/div/article/div[3]/div[1]/ul/li/div/button/span").click()                                     #yorumlardaki + tuşuna basıyor
 tm.sleep(5)  
 
 liste = []
 süreler = [4,5,7,8,4,5,4,6,4,5,5,6,4,5,5,4,4,8,8,5,4,7,5]
-for a in range(5,24):
-    liste.append(driver.find_element_by_xpath("/html/body/div[5]/div[2]/div/article/div[3]/div[1]/ul/ul[{}]/div/li/div/div[1]/div[2]/span/a".format(a)).text)
+for a in range(3,24):
+    liste.append(driver.find_element_by_xpath("/html/body/div[5]/div[2]/div/article/div[3]/div[1]/ul/ul[{}]/div/li/div/div[1]/div[2]/span".format(a)).text)
 
+
+"""BİRİNCİ = 0
+ÜCÜNCÜ = 0
+for char in liste[0]:
+    BİRİNCİ= BİRİNCİ+1
+for char in liste[2]:
+    ÜCÜNCÜ= ÜCÜNCÜ+1
+
+
+j = []
+for char in range[0,BİRİNCİ-2]:
+    j[char] = liste[0][char]
+
+liste[0] = j
+
+n = []
+for char in range[0,ÜCÜNCÜ-2]:
+    n[char] = liste[2][char]
+liste[2] = n"""
+
+#liste.append("emojiler sıkıntı olduğu için o yorumları alamadım")
+"""liste.append("sikiyim bir yerde patladı yine bi sn")
+for t in range(1,10):
+    liste.append("vay"*t)
 print(liste)
-for i in range(0,23):
-    yorum_butonu = driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[3]/section[1]/span[2]/button').click()                      #xpath de sonda buttun olmayan kısımları clicklediğinde çalışmıyo, ayrıca yorum kısmına send_keys yapabilmek için önce yorum butonuna basılması gerekiyo
+#driver.find_element_by_xpath("/html/body/div[5]/div[3]/button").click()
+liste.pop(0)
+liste.pop(1)"""
+print(liste)
+tm.sleep(2)
+t=0
+for i in range(0,20):
+    yorum_butonu = driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[3]/section[1]/span[2]/button').click()  
+    tm.sleep(2)                                                                                                                     #xpath de sonda button olmayan kısımları clicklediğinde çalışmıyo, ayrıca yorum kısmına send_keys yapabilmek için önce yorum butonuna basılması gerekiyo
     driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[3]/section[3]/div/form/textarea').send_keys(liste[i])
+    tm.sleep(2)
     paylas_butonu = driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[3]/section[3]/div/form/button[2]').click()
     tm.sleep(süreler[i])
+    t = t + 1
+    if t%2 == 0:
+        driver.find_element_by_xpath("/html/body/div[5]/div[3]/button").click()
+        tm.sleep(2)
+        driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/div[3]/article/div[1]/div/div[2]/div[2]/a/div").click()
+        tm.sleep(3)
+
 
 """tm.sleep(3)    
 driver.execute_script("window.scrollTo(0, 500)")
